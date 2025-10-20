eae331eda04d28c6d892c43c4e2eb9af4dbf08b8
Giresh-458 <gireshvelaga@gmail.com>
2025-10-20 17:41:33 +0530

    added seed

--- models/SubjectsSchema.js
+++ models/SubjectsSchema.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const subjectSchema = new mongoose.Schema({
   subject_name: { type: String, required: true },
   credits: { type: Number, required: true },
-  semester: { type: Number, required: true, min:1,max:8 }
+  semester: { type: Number, required: true, min:1,max:8 },
+  Branch: {type: String, enum: ["CSE", "ECE", "AIDS"], default: null}
+
 });
 
 module.exports = mongoose.model("Subject", subjectSchema);
